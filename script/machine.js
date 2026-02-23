102358989c67b5932093acf445d279da930f324c
thisisyourfahi <fahiyanofficial@gmail.com>
2026-02-23 20:06:23 +0600

    funcionality upto card existence in a particular list

--- script/machine.js
+++ script/machine.js
@@ -1,5 +1,25 @@
 function calculateCount() {
-    totalCount.innerText = allCards.children.length;
-    interviewCount.innerText = interviewCards.length;
-    rejectedCount.innerText = rejectedCards.length;
+    totalCount.innerText = allCardsList.children.length;
+    interviewCount.innerText = interviewCardsList.length;
+    rejectedCount.innerText = rejectedCardsList.length;
+}
+
+function createCardInfo(clickedCard) {
+    console.log('create card object');
+    const title = clickedCard.querySelector('.title').innerText;
+    const role = clickedCard.querySelector('.role').innerText;
+    const location = clickedCard.querySelector('.location').innerText;
+    const time = clickedCard.querySelector('.time').innerText;
+    const salary = clickedCard.querySelector('.salary').innerText;
+    const sta = clickedCard.querySelector('.sta').innerText;
+    const des = clickedCard.querySelector('.des').innerText;
+
+    const card = {
+        title, role, location, time, salary, sta, des
+    }
+
+    // console.log('card created:', title, role, location, time, salary, sta, des);
+    return card;
+    
+
 }
--- script/script.js
+++ script/script.js
@@ -2,6 +2,51 @@ let totalCount = document.getElementById('total-count');
 let interviewCount = document.getElementById('interview-count');
 let rejectedCount = document.getElementById('rejected-count');
 
-let allCards = document.querySelector('.all-cards');
-let interviewCards = [], rejectedCards = [];
+let allCardsList = document.querySelector('.all-cards');
+let interviewCardsList = [], rejectedCardsList = [];
 calculateCount();
+
+let acSection = document.getElementById('all-cards');
+let icSection = document.getElementById('int-cards');
+let rcSection = document.getElementById('rej-cards');
+
+
+// event occured in acSection
+acSection.addEventListener('click', function(event) {
+    const clickedCard = event.target.closest('.card');
+    const clickedBtn = event.target.closest('.btn');
+
+    
+    // which button was clicked?
+    if (clickedBtn.classList.contains('btn-success')) {
+        const cardInfo = createCardInfo(clickedCard);
+
+        // does it exist on the interviewCardList?
+        const exist = interviewCardsList.find(item => item.title === cardInfo.title);
+        if (exist) {
+            console.log('exist in the intcardlist');
+        } else {
+            console.log('doesnt exist in the intcardlist');
+        }
+    } else if (clickedBtn.classList.contains('btn-error')) {
+        const cardInfo = createCardInfo(clickedCard);
+
+        // does it exist in the rejectedCardList?
+        const exist = rejectedCardsList.find(item => item.title === cardInfo.title);
+        if (exist) {
+            console.log('exist in the rejectedCardList');
+        } else {
+            console.log('doesnt exist in the rejectedCardList');
+        }
+    }
+})
+
+// event occured in icSection
+icSection.addEventListener('click', function(event) {
+    console.log('event happened in icSection');
+})
+
+// event occured in rcSection
+rcSection.addEventListener('click', function(event) {
+    console.log('event happened in rcSectino');
+})
